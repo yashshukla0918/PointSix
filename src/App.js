fdc2e8b16256cabe6bb2e5b1ad613e33afba2bc8
Yash Shukla <yshuklarazz@gmail.com>
2023-07-09 21:44:26 +0530

    Share file functionality is added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import axios from "axios";
 import Navbar from "./Components/Navbar";
 import './App.css'
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Modal } from "./Components/Modal";
 import ShareButton from "./Components/ShareButton";
 
 function App() {
 
   const [src, setSrc] = useState()
-
-
-
   
   function getRandomImage() {
     let random = Math.floor(Math.random() * 300) + 1;
     // setRandom(getRandomInt());
     let x = `https://picsum.photos/id/${random}/200/300`
     axios.get(x)
-      .then((res) => { if (res.status === 200) { setSrc(x) } })
+      .then((res) => {
+        if (res.status === 200) {
+          setSrc(x)
+        }
+      })
       .catch((err) => {
         getRandomImage()
         console.error(err);
@@ -66,13 +66,18 @@ function App() {
                 <img src={src} alt="Loading..." /><br />
                 <div className="row ">
                   <center>
-                  <div className="col-sm-2 p-0">
-                  <button className="btn btn-outline-dark my-2" onClick={getRandomImage}>
-                    <i class="fa fa-refresh" aria-hidden="true"></i>
-                  </button>
-                  </div>
-                    <div className="col-sm-10 p-0"><ShareButton url={src}/></div>
-                 
+                    <div className="col-sm-2 p-0">
+                      <button className="btn btn-outline-dark my-2" onClick={getRandomImage}>
+                        <i className="fa fa-refresh" aria-hidden="true"></i>
+                      </button>
+                    </div>
+                    <div className="col-sm-10 p-0">
+                      <ShareButton
+                        url={src}
+                        title={'Random Image'}
+                        description={'Random Image Assignment'}
+                      />
+                    </div>
                   </center>
                 </div>
               </div>
@@ -81,7 +86,6 @@ function App() {
           <div className="col-sm-3"></div>
         </div>
       </div>
-      <Modal  url={src} description={"Random Image Assignment"} title={"Random Image"} />
     </HelmetProvider>
 
   );
--- src/Components/ShareButton.js
+++ src/Components/ShareButton.js
@@ -1,20 +1,44 @@
 import React from 'react'
 
-const ShareButton = ({url}) => {
-   
-    const handleShare = async () =>{
+const ShareButton = ({ url, title, description}) => {
+
+    const handleShare = async () => {
+        let shareData = undefined
         try {
-            await navigator.share({
-                url:url
-            });
-            console.log('Image Shared Successfully...');
+
+            const response = await fetch(url);
+            const blob = await response.blob()
+            const image = [new File([blob], 'image.jpg', {
+                type: blob.type,
+            })]
+            shareData = {
+                url: url,
+                title: title,
+                text: description,
+                files: image
+            }
+
+            if (navigator.canShare) {
+                if (navigator.canShare(shareData)) {
+                    await navigator.share(shareData);
+                    console.log('Image Shared Successfully...');
+                }
+                else {
+                    console.log("Unable to share image");
+                }
+            }
+
+
         } catch (error) {
-            console.error('Error Sharing Image : ',error);
+            console.error('Error Sharing Image : ', error);
         }
+
+
+
     }
-    
+
     return (
-    <button className='btn btn-outline-success my-2 form-control' onClick={handleShare}>Share</button>
-  )
+        <button className='btn btn-outline-success my-2 form-control' onClick={handleShare}>Share</button>
+    )
 }
-export  default ShareButton
+export default ShareButton
