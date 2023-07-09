29404f5a2659f6eeb36588f9f7e8290d628442ed
Yash Shukla <yshuklarazz@gmail.com>
2023-07-09 15:51:19 +0530

    Initial Commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <div className="navbar-brand">Random Image</div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
