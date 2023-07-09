0a5c283fc5860afd487945c4e58ebec58f07ceb9
Yash Shukla <yshuklarazz@gmail.com>
2023-07-09 22:42:08 +0530

    new changes

--- src/Components/ShareButton.js
+++ src/Components/ShareButton.js
@@ -14,9 +14,10 @@ const ShareButton = ({ url, title, description }) => {
             const message = encodeURI(`${title} \n${description}\nLink to image : ${url}\n`);
 
             shareData = {
-                // url: message,
+                //url: message,
+                description:description,
                 title: title,
-                text: description,
+                text: message,
                 files: image
             }
 
@@ -42,7 +43,7 @@ const ShareButton = ({ url, title, description }) => {
     return (
         <>
             <BrowserView>
-                <button className='btn btn-primary form-control my-1'><FacebookShareButton image={url}  quote={title}><FacebookIcon size={20} round/> Share on Facebook</FacebookShareButton></button><br/>
+                <button className='btn btn-primary form-control my-1'><FacebookShareButton url={url}  quote={title}><FacebookIcon size={20} round/> Share on Facebook</FacebookShareButton></button><br/>
                 <button className='btn btn-success w-100 my-1'><WhatsappShareButton url={url} title={title}><WhatsappIcon size={20} round/> Share on WhatsApp</WhatsappShareButton></button><br/>
                 <button className='btn btn-info w-100 my-1'><TwitterShareButton  url={url}  title={title}><TwitterIcon size={20} round/> Share on Twitter</TwitterShareButton></button>
             </BrowserView>
